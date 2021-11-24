f64d1513d9589846b91d8a68094f9801ed63035e
dimashpk <shupimonstr@gmail.com>
2021-11-24 13:35:26 +0200

    15_auth_implementation: added common input responsive styles

--- client/src/pages/InfoTables/MainTable/TabsPanel/Filters/FiltersPopover/FilterItem/styles.tsx
+++ client/src/pages/InfoTables/MainTable/TabsPanel/Filters/FiltersPopover/FilterItem/styles.tsx
@@ -9,25 +9,5 @@ export const useStyles = makeStyles(theme => ({
   valueInput: {
     marginRight: 12,
     flexShrink: 0,
-    [theme.breakpoints.down(768)]: {
-      '&  input, &  label': {
-        fontSize: 12,
-        lineHeight: '14px',
-      },
-      '& input': {
-        padding: '15px 10px',
-        height: 42,
-        boxSizing: 'border-box',
-      },
-      '& label': {
-        transform: 'translate(14px, 15px) scale(1)',
-      },
-      '& fieldset': {
-        minInlineSize: 'auto',
-        '& legend > span': {
-          fontSize: 9,
-        },
-      },
-    },
   },
 }));
